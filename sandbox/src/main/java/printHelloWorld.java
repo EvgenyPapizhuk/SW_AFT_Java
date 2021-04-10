1c6680d8eb54b1e767ea28c74752ab66b86bd63b
EvgenyPapizhuk <st-at883@yandex.ru>
2021-04-10 19:01:48 +0400

    Добавлено вычисление расстояния между точками

--- sandbox/src/main/java/printHelloWorld.java
+++ sandbox/src/main/java/printHelloWorld.java
@@ -1,5 +1,11 @@
 public class printHelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+
+        System.out.println("Дистанция между точками: " + Point.distance(new Point(3, 4), new Point(4, 3)));
+        System.out.println("Дистанция между точками: " + Point.distance(new Point(3, 4), new Point(3, 4)));
+        System.out.println("Дистанция между точками: " + Point.distance(new Point(3, 4), new Point(10, 10)));
+
+
     }
 }
