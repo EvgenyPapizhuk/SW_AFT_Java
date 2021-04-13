bb79c37f2539742f836e44343d469e5f672500d6
EvgenyPapizhuk <st-at883@yandex.ru>
2021-04-13 21:36:48 +0400

    Исправлен ответ на задание № 2

--- sandbox/src/main/java/printHelloWorld.java
+++ sandbox/src/main/java/printHelloWorld.java
@@ -2,9 +2,10 @@ public class printHelloWorld {
     public static void main(String[] args) {
 //        System.out.println("Hello World!");
 
-        System.out.println("Дистанция между точками: " + Point.distance(new Point(3, 4), new Point(4, 3)));
-        System.out.println("Дистанция между точками: " + Point.distance(new Point(3, 4), new Point(3, 4)));
-        System.out.println("Дистанция между точками: " + Point.distance(new Point(3, 4), new Point(10, 10)));
+        Point sample = new Point(3, 4);
+        System.out.println("Дистанция между точками: " + sample.distance(new Point(4, 3)));
+        System.out.println("Дистанция между точками: " + sample.distance(new Point(3, 4)));
+        System.out.println("Дистанция между точками: " + sample.distance(new Point(10, 10)));
 
 
     }
