da93c9a4ba3c6ce4f74cd9d86004ebb074b44d84
EvgenyPapizhuk <st-at883@yandex.ru>
2021-04-13 22:41:00 +0400

    Ответ на задание № 3
    + отключён грэдл

---
+++ sandbox/src/test/java/ru/aftlessons/sandbox/PointTest.java
@@ -0,0 +1,44 @@
+package ru.aftlessons.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest {
+
+    @Test
+    public void test (){
+        Point t = new Point(3,4);
+        assert t.x == 2;
+        System.out.println("Тест № 1 пройден");
+    }
+
+    @Test
+    public void test2 (){
+        Point t = new Point(3,4);
+        Assert.assertEquals( t.distance(new Point(3,4)) , 2);
+        System.out.println("Тест № 2 пройден");
+    }
+
+    @Test
+    public void test3 (){
+        Point t = new Point(3,4);
+        Assert.assertEquals( t.distance(new Point(3,4)) , 0);
+        System.out.println("Тест № 3 пройден");
+    }
+
+    @Test
+    public void test4 (){
+        Point t = new Point(3,4);
+        Assert.assertEquals( t.distance(new Point(3,3)) , 1);
+        System.out.println("Тест № 4 пройден");
+    }
+
+    @Test
+    public void test5 (){
+        Point t = new Point(3,4);
+        Assert.assertEquals( t.distance(new Point(4,5)) , 1.4142135623730951);
+        System.out.println("Тест № 5 пройден");
+    }
+
+
+}
