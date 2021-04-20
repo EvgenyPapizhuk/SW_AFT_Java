a782a686064c5bd4e1f9ac18d68b8038d2ced3cb
EvgenyPapizhuk <st-at883@yandex.ru>
2021-04-20 22:26:57 +0400

    Оптимизирован тест
    Авторизация вынесена в BeforeMethod

--- addressbook-web-tests/src/test/java/ru/stqa/lsft/addressbook/UntitledTestCase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lsft/addressbook/UntitledTestCase.java
@@ -13,10 +13,6 @@ public class UntitledTestCase {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testUntitledCase() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -27,6 +23,11 @@ public class UntitledTestCase {
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.id("LoginForm")).submit();
     wd.findElement(By.id("content")).click();
+  }
+
+  @Test
+  public void testUntitledCase() throws Exception {
+
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
