ac2f6b4551b420df68b64ee5a93753bdbe71b62f
EvgenyPapizhuk <st-at883@yandex.ru>
2021-05-05 22:01:00 +0400

    добавлено сравнение списков групп в тест на удаление

--- addressbook-web-tests/src/test/java/ru/stqa/lsft/addressbook/model/DateTestGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lsft/addressbook/model/DateTestGroup.java
@@ -1,9 +1,31 @@
 package ru.stqa.lsft.addressbook.model;
 
+import java.util.Objects;
+
 public class DateTestGroup {
     private final String name;
     private final String header;
     private final String footer;
+
+    @Override
+    public String toString() {
+        return "DateTestGroup{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DateTestGroup that = (DateTestGroup) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 
     public DateTestGroup(String name, String header, String footer) {
 //        this.name = "name" + (int)(Math.random()*10);
--- addressbook-web-tests/src/test/java/ru/stqa/lsft/addressbook/tests/testOfGroup/DeletedTestGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lsft/addressbook/tests/testOfGroup/DeletedTestGroup.java
@@ -16,11 +16,16 @@ public class DeletedTestGroup extends TestBase {
       app.getGroupHelper().createGroup(new DateTestGroup("test1", null, null));
     }
     List<DateTestGroup> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup(1);
+    app.getGroupHelper().selectGroup(before.size());
     app.getGroupHelper().initDeletedGroup();
     app.getNavigationHalper().goToGroup();
     List<DateTestGroup> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(before.size() - 1, after.size());
+
+    before.remove(before.size()-1);
+      Assert.assertEquals(before, after);
+
+
     app.exit1();
   }
 
