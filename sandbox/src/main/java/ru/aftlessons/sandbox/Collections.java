a1ac2bacb2300e30e090e07f36b417314ac53e6d
EvgenyPapizhuk <st-at883@yandex.ru>
2021-05-05 20:01:05 +0400

    Записан пример работы с коллекцией(List)

---
+++ sandbox/src/main/java/ru/aftlessons/sandbox/Collections.java
@@ -0,0 +1,20 @@
+package ru.aftlessons.sandbox;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+    public static void main(String[] args) {
+        String[] lansss = {"12", "23"} ;
+
+        List<String> languages = Arrays.asList("sd", "sdf");
+        for (String l : languages){
+            System.out.println(l);
+
+        }
+        languages.add("........");
+        languages.size();
+
+    }
+
+}
