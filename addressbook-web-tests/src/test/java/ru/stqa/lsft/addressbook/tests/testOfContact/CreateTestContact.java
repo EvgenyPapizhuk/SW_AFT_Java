029814fe1e8d6aca9af4fb93063bfd3778ad3d1c
EvgenyPapizhuk <st-at883@yandex.ru>
2021-05-10 13:13:19 +0400

    +сравнение списков в создание контактов

--- addressbook-web-tests/src/test/java/ru/stqa/lsft/addressbook/tests/testOfContact/CreateTestContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lsft/addressbook/tests/testOfContact/CreateTestContact.java
@@ -1,15 +1,31 @@
 package ru.stqa.lsft.addressbook.tests.testOfContact;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.lsft.addressbook.model.DateTestContact;
+import ru.stqa.lsft.addressbook.model.DateTestGroup;
 import ru.stqa.lsft.addressbook.tests.TestBase;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class CreateTestContact extends TestBase {
 
 
     @Test
     public void testCreateContact() throws Exception {
-        app.getContactHelper().createContact(new DateTestContact("test1", "test2", "test", "test1"), true);
+        app.getNavigationHalper().goToHome();
+        List<DateTestContact> before = app.getContactHelper().getContactList();
+        DateTestContact dataContact = new DateTestContact("test1", "test2", "test", "test1");
+        app.getContactHelper().createContact(dataContact, true);
+
+        List<DateTestContact> after = app.getContactHelper().getContactList();
+        before.add(dataContact);
+
+        Comparator<? super DateTestContact> byId = (o1, o2) -> Integer.compare(o1.hashCode(), o2.hashCode());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
         app.exit1();
     }
 
