c4b76881b59e56c6e809a9db929e0de102d66f6d
EvgenyPapizhuk <st-at883@yandex.ru>
2021-05-22 21:55:20 +0400

    Добавлена параметризация метода для создания группы

--- addressbook-web-tests/src/test/java/ru/stqa/lsft/addressbook/tests/testOfGroup/CreateTestGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lsft/addressbook/tests/testOfGroup/CreateTestGroup.java
@@ -1,28 +1,42 @@
 package ru.stqa.lsft.addressbook.tests.testOfGroup;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.lsft.addressbook.model.DateTestGroup;
 import ru.stqa.lsft.addressbook.model.Groups;
 import ru.stqa.lsft.addressbook.tests.TestBase;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.junit.MatcherAssert.*;
 
 public class CreateTestGroup extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new DateTestGroup().withName("test 1").withHeader("header 1").withFooter("footer 1")});
+    list.add(new Object[] {new DateTestGroup().withName("test 2").withHeader("header 2").withFooter("footer 2")});
+    list.add(new Object[] {new DateTestGroup().withName("test 3").withHeader("header 3").withFooter("footer 3")});
+    return list.iterator();
+  }
 
-  @Test
-  public void testUntitledCase() throws Exception {
-    app.goTo().groupPage();
-    Groups before = app.group().all();
-    DateTestGroup group1 = new DateTestGroup().withName("test2");
-    app.group().create(group1);
-    assertThat(before.size()+1, equalTo(app.group().count()));
-    Groups after = app.group().all();
-    assertThat(before.size() + 1, equalTo(after.size()));
 
-    assertThat(after, equalTo(
-            before.withAdded(group1.withtId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+  @Test (dataProvider = "validGroups")
+  public void testUntitledCase(DateTestGroup group1) throws Exception {
+//      DateTestGroup group1 = new DateTestGroup().withName(name).withHeader(header).withFooter(footer);
+      app.goTo().groupPage();
+      Groups before = app.group().all();
+      app.group().create(group1);
+      assertThat(before.size()+1, equalTo(app.group().count()));
+      Groups after = app.group().all();
+      assertThat(before.size() + 1, equalTo(after.size()));
+
+      assertThat(after, equalTo(
+              before.withAdded(group1.withtId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
   @Test
