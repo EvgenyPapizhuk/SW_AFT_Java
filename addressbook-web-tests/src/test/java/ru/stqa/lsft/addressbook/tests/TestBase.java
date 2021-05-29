c2a406804b5e3568903676bc62a554e1e192ab2c
EvgenyPapizhuk <st-at883@yandex.ru>
2021-05-29 22:32:26 +0400

    Добавлено логирование

--- addressbook-web-tests/src/test/java/ru/stqa/lsft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lsft/addressbook/tests/TestBase.java
@@ -1,16 +1,24 @@
 package ru.stqa.lsft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.lsft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.lsft.addressbook.tests.testOfGroup.CreateTestGroup;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
 
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
     protected static final ApplicationManager app
-            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+            = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
@@ -21,5 +29,16 @@ public class TestBase {
     public void tearDown() throws Exception {
         app.stop();
     }
+
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + "with parametr" + Arrays.asList(p));
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
+    }
+
 
 }
