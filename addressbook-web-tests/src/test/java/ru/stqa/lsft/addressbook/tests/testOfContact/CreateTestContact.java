17cb9eb2659f7a9c0f038ea466d49b3b36efbc3f
EvgenyPapizhuk <st-at883@yandex.ru>
2021-05-29 19:01:17 +0400

    Параметризован тест создания контактов

--- addressbook-web-tests/src/test/java/ru/stqa/lsft/addressbook/tests/testOfContact/CreateTestContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lsft/addressbook/tests/testOfContact/CreateTestContact.java
@@ -1,41 +1,77 @@
 package ru.stqa.lsft.addressbook.tests.testOfContact;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.lsft.addressbook.model.Contacts;
 import ru.stqa.lsft.addressbook.model.ContactDate;
+import ru.stqa.lsft.addressbook.model.DateTestGroup;
 import ru.stqa.lsft.addressbook.tests.TestBase;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
 
 public class CreateTestContact extends TestBase {
 
+    @DataProvider
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xStream = new XStream();
+        xStream.processAnnotations(ContactDate.class);
+        List<ContactDate> contacts = (List<ContactDate>) xStream.fromXML(xml);
+        return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
 
-    @Test
-    public void testCreateContact() throws Exception {
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactDate> contacts = gson.fromJson(json, new TypeToken<List<ContactDate>>() {}.getType());
+        return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+
+
+    @Test (dataProvider = "validContactsFromXml")
+    public void testCreateContact(ContactDate dataContact) throws Exception {
         app.goTo().goToHome();
         Contacts before = app.contact().all();
         File photo = new File("src/test/resources/shrimp.jpg");
-        ContactDate dataContact = new ContactDate()
-                .withtFirstName("test1").withtMiddleName("test2").withttLastName("test").withPhoto(photo);
-        app.contact().createContact(dataContact, true);
+//        ContactDate dataContact = new ContactDate()
+//                .withtFirstName("test1").withtMiddleName("test2").withttLastName("test").withPhoto(photo);
+        app.contact().createContact(dataContact.withPhoto(photo), true);
 
         Contacts after = app.contact().all();
 
         Comparator<? super ContactDate> byHC = (o1, o2) -> Integer.compare(o1.hashCode(), o2.hashCode());
-//        before.sort(byHC);
-//        after.sort(byHC);
-//        Assert.assertEquals(before, after);
         System.out.println("before: " + before.size());
         System.out.println("after: " + after.size());
-//        assertThat(after,equalTo(before));
         assertThat(after.sort1(byHC),equalTo(before.withAdded(dataContact).sort1(byHC)));
     }
 
-    @Test
+    @Test(enabled = false)
     public void testCurrentDir() {
         File currentDir = new File(".");
         System.out.println(currentDir.getAbsolutePath());
