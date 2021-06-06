c926ac53ea3ce509fb3667bf9a8750f56b2db8ab
EvgenyPapizhuk <st-at883@yandex.ru>
2021-06-06 22:50:11 +0400

    отмена капчи работает!

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -1,7 +1,12 @@
 <?php
-$g_hostname = 'localhost';
-$g_db_type = 'mysql';
-$g_database_name = 'bugtracker';
-$g_db_username = 'root';
-$g_db_password = '';
-$g_signup_use_captcha = OFF;
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'jG2D9BTyvrhzPn/NpLcTXIb/dl/rpCpgBs1OOHk691A=';
+
+$g_signup_use_captcha	= OFF;
